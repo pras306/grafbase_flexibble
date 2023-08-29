957238ce316f6df2e27088b4c3e934ad1b7d79d8
pras306 <44067654+pras306@users.noreply.github.com>
2023-08-29 23:37:46 +0530

    Update User name requirements

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -2,7 +2,7 @@ import { g, auth, config } from '@grafbase/sdk';
 
 //@ts-ignore
 const User = g.model('User', {
-  name: g.string().length({ min: 2, max: 20 }),
+  name: g.string().length({ min: 3, max: 20 }),
   email: g.string().unique(),
   avatarUrl: g.url(),
   description: g.string().optional(),
